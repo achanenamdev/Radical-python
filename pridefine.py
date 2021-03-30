0daa7cd372983453f36402678cbb053bcb138acb
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-03-30 20:23:36 +0530

    Add files via upload

---
+++ pridefine.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 20:15:05 2021
+
+@author: Lenovo
+"""
+hw="hello world"
+print("main string:",hw)
+
+print("title fun:",hw.title())
+
+print("upper fun:",hw.upper())
+
+print("isdigit fun:",hw.isdigit())
+
+print("islower fun:",hw.islower())
