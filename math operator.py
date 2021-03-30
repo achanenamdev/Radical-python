46ee37240d086379cff0904c1c419eeca4542e9c
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-03-30 20:40:34 +0530

    Add files via upload

--- math operator.py
+++ math operator.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 28 13:00:00 2021
+Created on Tue Mar 30 20:29:43 2021
 
 @author: Lenovo
 """
-
 a=10
 
 #a=a+456
 a=a+300
-#a+=300
-print('value of a+=300 is ',a)
+#a=a+300
+print('value of a+=300is',a)
 
 #a=a-200
 a-=200
-print('value of a-=200 is',a)
+print('value of a-=200is',a)
 
 #a=a*2
 a*=2
-print('value of a*=2 is ',a)
+print('value of a*=2 is',a)
 
 #a=a/2
 a/=2
-print('value of a/=2 is ',a)
+print('value of a/=2 is',a)
 
 #a=a%2
 a%=2
-print('value of a%=2',a)
+print('value of a%=2 is',a)
+
+a1=b1=10
+print(a1)
+print(b1)
