f01ce3446df82d79f0a3704d709cb6addd2a2c8b
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-03-30 19:35:37 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 19:27:14 2021
+
+@author: Lenovo
+"""
+
+print("welcome to radical")
+
+a=1
+c=3
+
+#addition of 2nos
+result=a+c
+#print result
+print("addition of 2 nos:",result)
+
+print("hello world")
+
+a=("hello world")
+print(a)
