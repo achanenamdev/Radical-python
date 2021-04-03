682a7e4770ca8965ae0dabb3a2171961005fa5f4
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-04-03 13:28:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,38 @@
+
+list1=[10,15.5,'python','A',"abc"]
+
+list2=['xyz','ABC','xyz']
+
+print('list1=',list1)
+print('list2=',list2)
+
+print('type of list1 is:',type(list1))
+
+print('lenth of list1=',len(list))
+
+print('string slicing')
+#first element=list[0]
+print('list1=',list[0])
+print('list[1]=',list1[1])
+print('list1[2]=',list1[2])
+
+print('list1 =', list1[2])
+
+#LIST [STARTINDEX:ENDINDEX(EXCLUDING):INTERVAL]
+
+print('list[2:5]=',list[2:5])
+
+print('list[0:2]=',list[0:2])
+
+print('list[::2]=',list[::2])
+
+#list reverse without using function
+print('list reverse list1[::-1]',list[::-1])
+
+print('list built -in methods')
+
+print('max(list2)=',max(list2))
+print('min(list2)=',min(list2))
+
+list3=list1+list2
+print('list1+list2=',list3)
