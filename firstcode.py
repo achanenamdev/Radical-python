8e1da0722c54ccffdfe97e0662046fd09f905853
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-04-06 19:03:48 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,28 @@
+         firstcode
+
+print("welcome to radical")
+
+#variable declare
+a=1
+c=3
+
+#addition of 2 nos 
+result=a+c
+
+#print result
+print("addition of 2 nos :",result)
+print ("hello world")
+a="hello world"
+print(a)
+
+hw="hello world"
+
+print("main string:",hw)
+
+print("title fun:",hw.title())
+
+print("upper fun:",hw.upper())
+
+print("isdigit fun;",hw.isdigit())
+
+print("islower fun:",hw.islower())
