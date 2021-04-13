7fd8448bc737cf12cb170a7178be6e686e54d27c
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-04-13 15:29:25 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,35 @@
+                          Tuple
+#empty tuple
+a=()
+print(a)
+print('type(a)',type(a))
+print(len(a))
+
+b=(50)
+print(b)
+print('type(b)',type(b))
+#print(len(b))
+
+singleElementTuple=(50,)
+print(singleElementTuple)
+print('type(singleElementTuple)',type(singleElementTuple))
+print(len(singleElementTuple))
+
+c=(10,20,'python',20,10,10)
+print(c)
+print('type(c)=',type(c))
+print(len(c))
+len(c)
+
+#tuple.count(element)
+print('c.count(10)=',c.count(10))
+
+print('c.index("python")=',c.index('python')
+         
+print(c.index(20)=',c.index(20)
+?index
+
+
+#delete tuple
+del 'a'
+print(a)
