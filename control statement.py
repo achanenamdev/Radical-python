85f8b92ca94fee8452b819e168149da0d2b1fd53
achanenamdev <81506008+achanenamdev@users.noreply.github.com>
2021-04-19 16:42:49 +0530

    Add files via upload

---
+++ control statement.py
@@ -0,0 +1,45 @@
+# control statement
+
+# if statement 
+temp = 40 
+if temp >= 40:
+    print ("room is hot")
+    
+    
+age = 17
+    
+print ('voting system') 
+if age > 17:
+    print('congrats !!! you are eligible for voting')
+else:
+    print('oh sorry!!! you are not eligible for voting process')
+        
+    
+age = 35
+if age < 22:
+    print("study hard !!! ")
+elif age >21 and age <26:
+    print("your salary shoud be in the range of 10k - 50k")
+elif age >25 and age <36:
+    print("your salary shoud be in the range of 50k - 2l") 
+elif age >35 and age<50:
+    print("your salary shoud be in the range of 2ll - 5l")
+else:
+    print("no need to work ...enjoy the life ....!!!!")
+    
+ 
+# python program to check whether the given number is even or odd.
+number =input("enter a number")
+
+X = int(number)%2
+
+if  X == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+    
+    
+    
+    
+    
+    
